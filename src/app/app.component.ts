d627154cfee11701086a013ce1c4d394afe94132
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-12-18 22:09:56 +0100

    scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './landingPage/landingPage.component';
+import { MainContentComponent } from "./main-content/main-content.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { OurMenuComponent } from './main-content/our-menu/our-menu.component';
+import { HowToOrderComponent } from './main-content/how-to-order/how-to-order.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, OurMenuComponent, HowToOrderComponent, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/landing-page/landingPage.component.ts
+++ src/app/main-content/landing-page/landingPage.component.ts
@@ -6,13 +6,7 @@ import { NavbarComponent } from "./navbar/navbar.component";
   selector: 'app-landingPage',
   standalone: true,
   imports: [CommonModule, NavbarComponent],
-  template: `
-  <section>
-    <div><app-navbar></app-navbar></div>
-    <h1 class="fontRaleway">SAKURA RAMEN</h1>
-    <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
-  </section>
-  `,
+  templateUrl: './landingPage.component.html',
   styleUrls:['./landingPage.component.scss']
 })
 export class LandingPageComponent {
